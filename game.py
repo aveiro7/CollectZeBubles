c4c4bf6cef979e267a56bf6cd13925d1d83e99ce
Olga Rosik-Rosinska <rosik@arviem.com>
2019-04-12 13:21:24 +0200

    Drawn player ship.

--- game.py
+++ game.py
@@ -2,17 +2,24 @@ from tkinter import *
 
 
 class Game(Canvas):
+    __GAME_WINDOW_WIDTH = 800
+    __GAME_WINDOW_HEIGHT = 600
 
     def __init__(self, master):
         super().__init__(master,
-                         width=800,
-                         height=600,
+                         width=self.__GAME_WINDOW_WIDTH,
+                         height=self.__GAME_WINDOW_HEIGHT,
                          bg="darkblue")
         self.pack()
 
     def start(self):
-        self.mainloop()
         print("Game started")
+        self.draw_ship()
+        self.mainloop()
+
+    def draw_ship(self):
+        self.create_polygon(5, 5, 5, 25, 30, 15, fill='green')
+        self.create_oval(0, 0, 30, 30, outline='green')
 
 
 game = Game(Tk())
