369e308008b5edb36c4bbc3d4854c762ed6b0b04
Olga Rosik-Rosinska <rosik@arviem.com>
2019-04-12 14:22:47 +0200

    Added moving logic to the bubble class.

--- bubble.py
+++ bubble.py
@@ -5,3 +5,6 @@ class Bubble:
     def __init__(self, canvas, color="white"):
         self.canvas = canvas
         self.outline = canvas.create_oval(0, 0, 100, 100, outline=color)
+
+    def move(self, offset_x, offset_y):
+        self.canvas.move(self.outline, offset_x, offset_y)
