f82105fe5ce3aa8df157799b65299d1f64bf832f
Olga Rosik-Rosinska <rosik@arviem.com>
2019-04-12 13:38:54 +0200

    Centered the ship on the canvas.

--- game.py
+++ game.py
@@ -15,11 +15,15 @@ class Game(Canvas):
     def start(self):
         print("Game started")
         self.draw_ship()
+        self.move_ship(self.__GAME_WINDOW_WIDTH / 2, self.__GAME_WINDOW_HEIGHT / 2)
         self.mainloop()
 
     def draw_ship(self):
-        self.create_polygon(5, 5, 5, 25, 30, 15, fill='green')
-        self.create_oval(0, 0, 30, 30, outline='green')
+        self.create_polygon(5, 5, 5, 25, 30, 15, fill='green', tags="ship")
+        self.create_oval(0, 0, 30, 30, outline='green', tags="ship")
+
+    def move_ship(self, x_offset, y_offset):
+        self.move("ship", x_offset, y_offset)
 
 
 game = Game(Tk())
